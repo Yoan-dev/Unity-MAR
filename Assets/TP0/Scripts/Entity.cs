841ea5fa791189c608e4ad8cc25a02950657eac9
Yoan-dev <yoanbocquelet@gmail.com>
2016-12-02 13:40:06 +0100

    finalisation TP0

---
+++ Assets/TP0/Scripts/Entity.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Entity : MonoBehaviour {
+
+	public GameObject sphere;
+
+	private bool rotate;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.R)) rotate = !rotate;
+		if (rotate) transform.Rotate(new Vector3(-0.75f, 0, 0));
+	}
+
+	public void ShadowsOnOff (bool value) {
+		ShadowOnOff (gameObject, value);
+		ShadowOnOff (sphere, value);
+	}
+
+	private void ShadowOnOff (GameObject obj, bool value) {
+		gameObject.GetComponent<MeshRenderer> ().receiveShadows = value;
+			
+	}
+}
---
+++ Assets/TP0/Scripts/UI.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI : MonoBehaviour {
+	
+	// UI
+	public UnityEngine.UI.Slider lightSlider;
+	public UnityEngine.UI.Toggle shadows;
+	public UnityEngine.UI.Button resetTransform;
+	public UnityEngine.UI.Toggle[] rotations;
+	public UnityEngine.UI.Dropdown type;
+
+	// Objects
+	public Light light;
+	public GameObject[] objects;
+	public GameObject current;
+
+	// Use this for initialization
+	void Start () {
+		lightSlider.onValueChanged.AddListener(delegate{UpdateLight();});
+		shadows.onValueChanged.AddListener(delegate{UpdateShadows();});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void UpdateLight() {
+		light.intensity = lightSlider.value;
+	}
+
+	private void UpdateShadows() {
+		for (int i = 0; i < 4; i++)
+			(objects [i].GetComponent<Entity> () as Entity).ShadowsOnOff (shadows.isOn);
+	}
+}
