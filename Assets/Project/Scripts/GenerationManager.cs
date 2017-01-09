72479f7d013672867cef35f57df9634e5e32682c
Yoan-dev <yoanbocquelet@gmail.com>
2017-01-09 11:29:05 +0100

    generation

--- Assets/Project/Scripts/GenerationManager.cs
+++ Assets/Project/Scripts/GenerationManager.cs
@@ -22,43 +22,14 @@ public class GenerationManager : MonoBehaviour {
 			Generate ();
 	}
 
-	private bool[,] map;
+	private bool[,] road;
 	private float[,] heights;
 
 	private void Generate () {
-		map = GenerateRoad ();
-		terrainData.SetHeights (0, 0, GenerateHeights (GenerateRoad ()));
+		road = GenerateRoad ();
+		heights = GenerateHeights ();
+		terrainData.SetHeights (0, 0, GenerateTerrain (road, heights));
 	}
-
-	void OnDrawGizmos()
-	{
-		if (map != null)
-		{
-			for (int i = 0; i < map.GetLength(0); i++)
-			{
-				for (int j = 0; j < map.GetLength(1); j++)
-				{
-					if (map [i, j])
-						Gizmos.color = Color.red;
-					else
-						Gizmos.color = Color.white;
-					
-					Vector2 pos = new Vector2(i, j);
-					Gizmos.DrawCube(pos, Vector2.one);
-				}
-			}
-		}
-	}
-
-		/*
-		float[,] heights = new float[(int)terrainData.size.x, (int)terrainData.size.z];
-		for (int i = 0; i < heights.GetLength (0); i++) {
-			for (int j = 0; j < heights.GetLength (1); j++) {
-				heights [i, j] = (float)(i + j) / (float)(heights.GetLength (0) + heights.GetLength (1)) * 0.15f;
-			}
-		}
-		terrainData.SetHeights (0, 0, heights);
-		*/
 
 	private bool[,] GenerateRoad () {
 		bool[,] res = new bool[(int)terrainData.size.x, (int)terrainData.size.z];
@@ -74,28 +45,86 @@ public class GenerationManager : MonoBehaviour {
 		return res;
 	}
 
+	private float[,] GenerateHeights () {
+		float[,] res = new float[(int)terrainData.size.x, (int)terrainData.size.z];
+		for (int i = 0; i < res.GetLength (0); i++) {
+			for (int j = 0; j < res.GetLength (1); j++) {
+				res [i, j] = 0.5f;
+			}
+		}
+		return res;
+	}
+
+	private float[,] GenerateTerrain (bool[,] road, float[,] heights) {
+		return null;
+	}
+
+	void OnDrawGizmos()
+	{
+		if (road != null)
+		{
+			for (int i = 0; i < road.GetLength(0); i++)
+			{
+				for (int j = 0; j < road.GetLength(1); j++)
+				{
+					if (road [i, j])
+						Gizmos.color = Color.red;
+					else
+						Gizmos.color = Color.white;
+					
+					Vector2 pos = new Vector2(i, j);
+					Gizmos.DrawCube(pos, Vector2.one);
+				}
+			}
+		}
+	}
+
+	/*
+
+	private IList<Vector2> outerRoads = new List<Vector2>();
+	private IList<Vector2> innerRoads = new List<Vector2>();
+
+	private bool[,] GenerateRoad () {
+		bool[,] res = new bool[(int)terrainData.size.x, (int)terrainData.size.z];
+		for (int i = 0; i < res.GetLength (0); i++) {
+			for (int j = 0; j < res.GetLength (1); j++) {
+				if (
+					((i < 40 || i > terrainData.size.x - 40) || (j < 40 || j > terrainData.size.z - 40)) &&
+					(i > 30 && i < terrainData.size.x - 30 && j > 30 && j < terrainData.size.z - 30)) {
+					res [i, j] = true;
+					if ((i == 39 || i == terrainData.size.x - 41) || (j == 39 || j == terrainData.size.z - 41) &&
+					    (i > 30 && i < terrainData.size.x - 30 && j > 30 && j < terrainData.size.z - 30))
+						outerRoads.Add (new Vector2 (i, j));
+					else
+						innerRoads.Add (new Vector2 (i, j));
+				}
+				else res [i, j] = false;
+			}
+		}
+		return res;
+	}
+
 	private float[,] GenerateHeights (bool[,] road) {
 		float[,] res = new float[(int)terrainData.size.x, (int)terrainData.size.z];
 		for (int i = 0; i < res.GetLength (0); i++) {
 			for (int j = 0; j < res.GetLength (1); j++) {
-				res [i, j] = ClosestRoad (road, i, j) / 320.0f;
+				res [i, j] = Mathf.Min(ClosestRoad (i, j) / 320.0f, 1.0f);
 			}
 		}
 		return res;
 	}
 
-	private float ClosestRoad (bool[,] road, int x, int y) {
-		float res = 9999;
-		for (int i = 0; i < road.GetLength (0); i++) {
-			for (int j = 0; j < road.GetLength (1); j++) {
-				if (road [i, j]) {
-					float dist = Mathf.Sqrt (Mathf.Pow (i - x, 2) + Mathf.Pow (j - y, 2));
-					if (dist < res)
-						res = dist;
-				}
-			}
-		}
+	private float ClosestRoad (int x, int y) {
+		if (VectorIn(x, y, innerRoads)) return 0;
 		return res;
 	}
 
+	private bool VectorIn (int x, int y, IList<Vector2> list) {
+		foreach (Vector2 vector in list)
+			if (vector.x == x && vector.y == y)
+				return true;
+			else
+				return false;
+	}
+	*/
 }
