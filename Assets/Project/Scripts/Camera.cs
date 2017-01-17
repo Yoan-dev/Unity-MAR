ed625187be1866a14ad70eae2bd7da6eb0bf1f78
Yoan <yoanbocquelet@gmail.com>
2017-01-17 12:47:27 +0100

    files refactoring, camera switching

---
+++ Assets/Project/Scripts/Camera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+    public GameObject otherCamera;
+	
+	void Update () {
+		if (Input.GetKeyUp(KeyCode.C))
+        {
+            otherCamera.SetActive(true);
+            gameObject.SetActive(false);
+        }
+	}
+}
