86668fcfdc76e5ccdb822cf280f9b2e7383e7ced
Grégory <greg.pennetier@gmail.com>
2017-01-22 19:03:25 +0100

    controls

--- Assets/Project/Scripts/Controls.cs
+++ Assets/Project/Scripts/Controls.cs
@@ -2,53 +2,65 @@
 using System.Collections;
 using System.Collections.Generic;
 
-[System.Serializable]
-public class AxleInfo {
-	public WheelCollider leftWheel;
-	public WheelCollider rightWheel;
-	public bool motor;
-	public bool steering;
-}
+public class Controls : MonoBehaviour
+{
+    public WheelCollider[] wheels;
+ 
+    private float enginePower = 2000f;
 
-public class Controls : MonoBehaviour {
-	public List<AxleInfo> axleInfos; 
-	public float maxMotorTorque;
-	public float maxSteeringAngle;
+    private float power = 0.0f;
+    private float brake = 0.0f;
+    private float steer = 0.0f;
 
-	// finds the corresponding visual wheel
-	// correctly applies the transform
-	public void ApplyLocalPositionToVisuals(WheelCollider collider)
-	{
-		if (collider.transform.childCount == 0) {
-			return;
-		}
+    private float maxSteer = 50.0f;
 
-		Transform visualWheel = collider.transform.GetChild(0);
+    void Start()
+    {
+       // GetComponent<Rigidbody>().centerOfMass = new Vector3(0,0,0);
+    }
 
-		Vector3 position;
-		Quaternion rotation;
-		collider.GetWorldPose(out position, out rotation);
+    void Update()
+    {
+        power = Input.GetAxis("Vertical") * enginePower * Time.deltaTime * 250.0f;
+        steer = Input.GetAxis("Horizontal") * maxSteer;
+        brake = Input.GetKey("m") ? GetComponent<Rigidbody>().mass * 0.1f : 0.0f;
 
-		visualWheel.transform.position = position;
-		visualWheel.transform.rotation = rotation;
-	}
+        wheels[0].steerAngle = steer;
+        wheels[1].steerAngle = steer;
+        Debug.Log("power " + wheels[2].motorTorque);
+        Debug.Log("steer " + wheels[0].steerAngle);
+        Debug.Log("brake " + wheels[0].brakeTorque);
 
-	public void FixedUpdate()
-	{
-		float motor = maxMotorTorque * Input.GetAxis("Vertical");
-		float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+        /*if(wheels[0].isGrounded)
+            Debug.Log(wheels[0].isGrounded);
+        if (wheels[1].isGrounded)
+            Debug.Log(wheels[1].isGrounded);
+        if (wheels[2].isGrounded)
+            Debug.Log(wheels[2].isGrounded);
+        if (wheels[3].isGrounded)
+            Debug.Log(wheels[3].isGrounded);*/
+        if (brake > 0.0f)
+        {
+            wheels[0].brakeTorque = brake;
+            wheels[1].brakeTorque = brake;
+            wheels[2].brakeTorque = brake;
+            wheels[3].brakeTorque = brake;
+            wheels[2].motorTorque = 0.0f;
+            wheels[3].motorTorque = 0.0f;
+        }
+        else
+        {
+            wheels[0].brakeTorque = 0f;
+            wheels[1].brakeTorque = 0f;
+            wheels[2].brakeTorque = 0f;
+            wheels[3].brakeTorque = 0f;
+            wheels[2].motorTorque = power;
+            wheels[3].motorTorque = power;
+            //GetComponent<Rigidbody>().AddTorque(Vector3.forward * power);
+        }
+    }
 
-		foreach (AxleInfo axleInfo in axleInfos) {
-			if (axleInfo.steering) {
-				axleInfo.leftWheel.steerAngle = steering;
-				axleInfo.rightWheel.steerAngle = steering;
-			}
-			if (axleInfo.motor) {
-				axleInfo.leftWheel.motorTorque = motor;
-				axleInfo.rightWheel.motorTorque = motor;
-			}
-			ApplyLocalPositionToVisuals(axleInfo.leftWheel);
-			ApplyLocalPositionToVisuals(axleInfo.rightWheel);
-		}
-	}
+    /*private WheelCollider GetCollider(int n){
+        return wheels[n].gameObject.GetComponent<WheelCollider>();
+    }*/
 }
